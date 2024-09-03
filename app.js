832bf03c7ad2598fe2a83d8c1c5a3e379f00f1a3
Fahima-anny <fahima.info03@gmail.com>
2024-09-04 02:49:26 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,244 @@
+// let array = [1, 2, 3, 4, 5, 6, 7]
+// for (let ar of array){
+//     console.log(ar)
+// }
+
+// let num = 10 ;
+// while(num<20){
+//     console.log(num) ;
+//     num= num+2
+// }
+// for (let x=1 ; x<=20 ; x++){
+//     console.log(x)
+// }
+
+// let sum = 0 
+// for(let i=0 ; i<=5; i++){
+//     sum = sum + i 
+// }
+// console.log(sum)
+
+// let sum2 = 200  
+// for(let i=200 ; i<=250; i++){
+//     sum2 = sum2 + i 
+// }
+// console.log(sum2)
+
+// for(let i=20; i>=1; i--){
+//     console.log(i)
+// }
+
+// let num = 20 
+// while(num>=1){
+//     console.log(num)
+//     num--
+// }
+
+// for(let i=0 ; i<=30 ; i++){
+//     if(i%5===0){
+// console.log(i)
+//     }
+// }
+
+// let sum =0
+// for(let i=0 ; i<=30 ; i++){
+//     if(i%3===0){
+//         console.log(i)
+//         sum=sum+i
+//     }
+// }
+// console.log(sum)
+
+// for(let i=1 ; i<=10 ; i++){
+//     if(i>6){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// for(let i=1 ; i<=10 ; i++){
+//     if(i%2===0){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 5; i+=3) {
+//     console.log(i);
+//    }
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 1; i < array.length; i++) {
+//  if (i == 5) {
+//  continue;
+//  }
+//  console.log(array[i]);
+// }
+
+// for(let i=1 ; i<=200 ; i++){
+//     console.log(i)
+//     if(i===100){
+// break ;
+//     }
+// }
+
+// let num = 1
+// let sum = 0
+// while(num<100){
+//     sum = sum + num ;
+//     if(sum > 100){
+//         break ;
+//     }
+//     num++
+// }
+// console.log(sum)
+
+// for (let i = 1; i <= 100; i++) {
+//     if (Number.isInteger(Math.sqrt(i))) {
+//       console.log(`Encountered the first square number: ${i}`);
+//       break;
+//     }
+//     console.log(i);
+//   }
+
+// for(let i=1 ; i<=100 ; i++){
+// if(i%2===1){
+//     continue ;
+// }
+// else(console.log(i))
+// }
+
+// let i = 55 ;
+// while(i<=85){
+//             if(i%5!==0){
+//                 console.log(i)  
+//     }
+//     i=i+2
+// }
+
+// for(let i=55 ; i<=85 ; i++){
+// if(i%2!==0){
+//     if(i%5===0){
+//         continue
+//     }
+//     console.log(i)
+// }
+// }
+
+// for( let i=0 ; i<=60 ; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!")
+// }
+
+// for(let i=61 ; i<=100 ; i++){
+//     if(i%2===1){
+//         console.log(i)
+//     }
+// }
+
+// for(let i=78 ; i<=98 ; i++){
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
+
+// let sum =0 
+// for(let i=91 ; i<=129 ; i++){
+//     if(i%2!==0){
+//         sum = sum + i 
+//     }
+// }
+// console.log(sum)
+
+// let sum = 0
+// for(let i = 51 ; i <= 85 ; i++ ){
+//     if(i%2===0){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum)
+
+// let multiplication = 1
+// for(let i=1 ; i<=10 ; i++){
+//   let  multiplication = 9 * i
+// console.log(`${i} x 9 = ${multiplication}`)
+// }
+
+// for(let i=81 ; i>=65 ; i--){
+//     console.log(i)
+// }
+
+// let i=0
+// while(i<60){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!",i)
+//     i++
+// }
+
+// let i = 61
+// while(i<=100){
+// if(i%2!==0){console.log(i)}
+//     i++
+// }
+
+// let i = 78
+// while(i<=98){
+// if(i%2===0){
+//     console.log(i)
+// }
+//     i++
+// }
+
+// let sum = 0
+// let i=81
+// while(i<=131){
+// sum = sum + i
+//     i=i+2
+// }
+// console.log(sum)
+
+// let sum = 0
+// let i = 206
+// while(i<=311){
+// sum = sum+i
+//     i=i+2
+// }
+// console.log(sum)
+
+// let i = 1
+// while(i<=10){
+//   let  multi = 5*i
+//   console.log(`${i} x 5 = ${multi}`)
+//   i++
+// }
+
+let i = 21
+while(i>=15){
+console.log(i)
+    i--
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
